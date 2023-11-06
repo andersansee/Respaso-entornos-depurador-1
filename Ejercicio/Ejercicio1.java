06f6369e20ee4f6b8c15b3d52f64477786665dfa
JaimeManero <jmaneromo23dw@ikzubirimanteo.com>
2023-11-06 13:39:59 +0100

    corregir ejercicio

--- Ejercicio/Ejercicio1.java
+++ Ejercicio/Ejercicio1.java
@@ -6,39 +6,34 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        
+
         List<Integer> lista = new ArrayList<Integer>();
 
         int entrada;
 
         Scanner sc = new Scanner(System.in);
 
-        //Mientras que la entrada sea mayor que 0, se añade a la lista si es par
-        while((entrada = sc.nextInt()) > 0)
-        {
-            if(entrada%2==0)
-            {
+        // Mientras que la entrada sea mayor que 0, se añade a la lista si es par
+        while ((entrada = sc.nextInt()) > 0) {
+            if (entrada % 2 == 0) {
                 lista.add(entrada);
             }
         }
 
-        for(int i=0;i<lista.size();i++)
-        {
-            for(int j=0;j<lista.size()-1;j++)
-            {
-                if(lista.get(j)>lista.get(i))
-                {
+        for (int i = 0; i < lista.size(); i++) {
+            for (int j = 0; j < lista.size() - 1; j++) {
+                if (lista.get(j) > lista.get(j + 1)) {
                     int aux = lista.get(j);
-                    lista.set(j, lista.get(j));
-                    lista.set(j+1, aux);
+                    lista.set(j, lista.get(j + 1));
+                    lista.set(j + 1, aux);
                 }
             }
         }
 
-        for(int i=0;i<lista.size();i++)
-        {
+        for (int i = 0; i < lista.size(); i++) {
             System.out.println(lista.get(i));
         }
 
+        sc.close(); // Cierra el Scanner al final para evitar pérdida de recursos.
     }
 }
